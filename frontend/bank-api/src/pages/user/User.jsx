c767978ff01438a3ce19fbedc9f2051944fcaf8e
Vincent Muller <v.muller55000@gmail.com>
2023-07-10 14:17:33 +0200

    account component

--- frontend/bank-api/src/pages/user/User.jsx
+++ frontend/bank-api/src/pages/user/User.jsx
@@ -2,11 +2,15 @@ import { useDispatch, useSelector } from "react-redux"
 import { useState } from "react"
 import editUser from "../../services/user"
 import { profileActions } from "../../store/profile"
+import { Link } from "react-router-dom"
+import Account from "../../components/account/Account"
+
 
 function User () {
     /**
      * Use selector to get states
      */
+    const isAuth = useSelector((state) => state.auth.isAuth);
     const userData = useSelector((state) => state.profile);
     const token = useSelector((state) => state.token.token);
     /**
@@ -31,60 +35,44 @@ function User () {
     };
 
     return (
-        <main className="main bg-dark">
-            {displayForm && (
-                <div className="header">
-                    <h1>Welcome back</h1>
-                    <div className="containeEditName">
-                        <div className="editNameInput">
-                            <input type="text" placeholder={userData.firstName} onChange={(e) => setFirstName(e.target.value)}/>
-                            <input type="text" placeholder={userData.lastName} onChange={(e) => setLastName(e.target.value)}/>
-                        </div>
-                        <div className="buttonBox">
-                            <button onClick={handleForm}>Save</button>
-                            <button onClick={handleDisplayForm}>Cancel</button>
-                        </div>
+        <main className="main bg-dark"> 
+            {isAuth? (
+                <div>
+                    {displayForm && (
+                        <div className="header">
+                            <h1>Welcome back</h1>
+                            <div className="containeEditName">
+                                <div className="editNameInput">
+                                    <input type="text" placeholder={userData.firstName} onChange={(e) => setFirstName(e.target.value)}/>
+                                    <input type="text" placeholder={userData.lastName} onChange={(e) => setLastName(e.target.value)}/>
+                                </div>
+                                <div className="buttonBox">
+                                    <button onClick={handleForm}>Save</button>
+                                    <button onClick={handleDisplayForm}>Cancel</button>
+                                </div>
+                            </div>
                     </div>
-            </div>
-            )}
-            {!displayForm && (
-                <div className="header">
-                <h1>Welcome back<br />{userData.firstName} {userData.lastName}</h1>
-                <button className="edit-button" onClick={handleDisplayForm}>Edit Name</button>
-            </div>
-            )}
-            <h2 className="sr-only">Accounts</h2>
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-                    <p className="account-amount">$2,082.79</p>
-                    <p className="account-amount-description">Available Balance</p>
+                    )}
+                    {!displayForm && (
+                        <div className="header">
+                        <h1>Welcome back<br />{userData.firstName} {userData.lastName}</h1>
+                        <button className="edit-button" onClick={handleDisplayForm}>Edit Name</button>
+                    </div>
+                    )}
+                    <h2 className="sr-only">Accounts</h2>
+                    <Account title='Argent Bank Checking (x8349)' amount="$2,082.79" description="Available Balance"/>
+                    <Account title='Argent Bank Savings (x6712)' amount="$10,928.42" description="Available Balance"/>
+                    <Account title='Argent Bank Credit Card (x8349)' amount="$184.30" description="Current Balance"/>
                 </div>
-                <div className="account-content-wrapper cta">
-                    <button className="transaction-button">View transactions</button>
+            ) : 
+            (
+                <div>
+                    <h2>You need to be login</h2>
+                    <Link to={'/'}> HomePage </Link>
                 </div>
-            </section>
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-                    <p className="account-amount">$10,928.42</p>
-                    <p className="account-amount-description">Available Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className="transaction-button">View transactions</button>
-                </div>
-            </section>
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-                    <p className="account-amount">$184.30</p>
-                    <p className="account-amount-description">Current Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className="transaction-button">View transactions</button>
-                </div>
-            </section>
+            ) }
         </main>
+        
     )
 }
 
